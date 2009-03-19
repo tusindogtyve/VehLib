1f1c18d74e433c7f1e63005ff97172738d3f9cfb
jensbmadsen <jensbmadsen@2ce8e84b-58be-4b20-b7a8-e71665c451d3>
2009-03-19 21:00:55 +0000

    reverted to 2008r1
    git-svn-id: svn+ssh://svn.vehlib.dk/var/SDlib/svn@80 2ce8e84b-58be-4b20-b7a8-e71665c451d3

--- trunk/DevTools/SD_aviewDev.bat
+++ trunk/DevTools/SD_aviewDev.bat
@@ -1,4 +1,4 @@
-set MDI_CMD=adamsmdr4 aview ru-s i e
+set MDI_CMD=adams08r1 aview ru-s i e
 set MDI_SUBMODEL=1
 set MDI_SD_LIBRARY_SITE=C:\Documents and Settings\jensbmadsen\My Documents\Library Project\Source\Adams_library
 set MDI_SD_DEVTOOLS=C:\Documents and Settings\jensbmadsen\My Documents\Library Project\Source\DevTools
--- trunk/UserInstall/SD_aviewUser.bat
+++ trunk/UserInstall/SD_aviewUser.bat
@@ -1,4 +1,4 @@
-set MDI_CMD=adamsmdr4 aview ru-s i
+set MDI_CMD=adams08r1 aview ru-s i
 set MDI_SUBMODEL=1
 set MDI_SD_LIBRARY_SITE=C:\Documents and Settings\jensbmadsen\My Documents\Library Project\Source\Adams_library
 set MDI_USER_PLUGIN_DIR=C:\Documents and Settings\jensbmadsen\My Documents\Library Project\Source\Adams_library\myplugins
